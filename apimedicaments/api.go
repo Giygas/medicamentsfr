0046b6abc27e0a7f98c0f004ecb427037f7e29d7
Giygas <gustavorossich@gmail.com>
2023-08-17 15:57:56 +0200

    removed testing functions

--- apimedicaments/api.go
+++ apimedicaments/api.go
@@ -5,31 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"runtime"
 	"time"
 
 	"github.com/giygas/medicamentsfr/medicamentsparser"
 	"github.com/juju/ratelimit"
-	"github.com/jasonlvhit/gocron"
 )
 
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	fmt.Printf("Alloc = %v MiB", m.Alloc / 1024 / 1024)
-	fmt.Printf("\tTotalAlloc = %v MiB", m.TotalAlloc / 1024 / 1024)
-	fmt.Printf("\tSys = %v MiB", m.Sys / 1024 / 1024)
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func executeCronJob() {
-	gocron.Every(1).Second().Do(PrintMemUsage)
-	<- gocron.Start()
-}
-
-
 func main() {
-	go executeCronJob()
 	
 	medicamentsparser.ParseAllMedicaments()
 	
